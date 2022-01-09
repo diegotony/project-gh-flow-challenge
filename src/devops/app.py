1c33d1815dd5571fa9ea7410c20ae7b91c6839e1
Diego Castillo <diegotony1396@gmail.com>
2022-01-09 00:04:40 -0500

    feat: api key integration

--- src/devops/app.py
+++ src/devops/app.py
@@ -4,33 +4,44 @@ import logging
 
 
 def lambda_handler(event, context):
-    data = json.loads(event["body"])
-    if 'message' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't send message missing message")
-    if 'to' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't send message missing to ")
+    print(event)
+    if event["httpMethod"] == "POST":
+        data = json.loads(event["body"])
+        if 'message' not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't send message missing message")
+        if 'to' not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't send message missing to ")
 
-    if 'from' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't send message missing from")
+        if 'from' not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't send message missing from")
 
-    if 'timeToLifeSec' not in data:
-        logging.error("Validation Failed")
-        raise Exception("Couldn't send message missing timeToLifeSec")
+        if 'timeToLifeSec' not in data:
+            logging.error("Validation Failed")
+            raise Exception("Couldn't send message missing timeToLifeSec")
 
 
-    item = {
-        "message": "Hello {0} your message will be send".format({data["to"]})
-    }
+        item = {
+            "message": "Hello {0} your message will be send".format(str(data["to"]))
+        }
 
-    response = {
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "statusCode": 200,
-        "body": json.dumps(item)
-    }
+        response = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "statusCode": 200,
+            "body": json.dumps(item)
+        }
 
-    return response
+        return response
+    else:
+        response = {
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "statusCode": 200,
+            "body": "ERROR"
+        }
+        return response
